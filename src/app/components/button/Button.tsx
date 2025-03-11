2b5121a4ff708e636894a195d90980e327a7c84f
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2025-03-10 21:31:48 -0400

    added hero featured

--- src/app/components/button/Button.tsx
+++ src/app/components/button/Button.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import styles from './Button.module.css';
 
 type ButtonProps = {
-  size?: "sm" | "md";
+  type?: "sm" | "md" | 'circle' | 'square';
   style?: React.CSSProperties;
   children: React.ReactNode;
 };
 
-export default function Button({ size = "md", style, children }: ButtonProps) {
+export default function Button({ type: size = "md", style, children }: ButtonProps) {
   return (
     <button className={`${styles.button} ${styles[size]}`} style={style}>
       {children}
--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className={styles.hero}>
       <div className={styles.main}>
         <div className={styles.topleft}>
-          <Button size="sm">Yoga Lifestyle</Button>
+          <Button type="sm" >Yoga Lifestyle</Button>
           <p>Find your <br /> INNER PEACE</p>
         </div>
         <div className={styles.bottomleft}>
@@ -17,7 +17,33 @@ const Hero = () => {
           <h1><span>Inner Peace</span><span>Elevated Living</span> </h1>
         </div>
       </div>
+      <article className={styles.featured}>
+        <Button type='sm'>Client</Button>
+        <p>Become a <br />REGULAR CUSTOMER</p>
+        <p>Step into <br />the Yoga Flow</p>
+        <div className={styles.bottomRight}>
+          <Button type='circle'>
+            <Arrow />
+          </Button>
+        </div>
+      </article>
+      <article className={styles.featured}>
+        <Button type='sm'>Product</Button>
+        <p>Create Your Practice <br /> as You Envisioned</p>
+        <p>{"Nature's Essence"} <br />Within</p>
+        <div className={styles.bottomRight}>
+          <Button type='circle'>
+            <Arrow />
+          </Button>
+        </div>
+      </article>
     </section>
+  );
+}
+
+const Arrow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.525 5.495l-.004 2l4.557.01l-9.603 9.585l1.413 1.415l9.63-9.61l-.012 4.614l2 .004l.02-8z" /></svg>
   );
 }
 
