18513aecfb66dcf3eb243d110ed1e8f561d5f098
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2025-04-06 15:36:37 -0400

    attempt to fixing assets  #2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = true;
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   distDir: isProd ? '/docs' : 'out',
