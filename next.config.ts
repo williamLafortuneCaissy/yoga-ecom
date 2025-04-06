43ec256df3a9518394008e12734ab899c9b970cc
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2025-04-06 16:05:26 -0400

    improved config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = true;
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   distDir: isProd ? '/docs' : 'out',
